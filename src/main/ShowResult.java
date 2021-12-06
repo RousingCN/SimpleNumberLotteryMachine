937f06d1287417eb13f60a711cbe21433494436a
RousingCN <2289049570@qq.com>
2021-12-06 09:28:44 +0800

    获奖者名单界面

--- src/main/ShowResult.java
+++ src/main/ShowResult.java
@@ -2,23 +2,22 @@ package main;
 
 import javax.swing.*;
 import java.awt.*;
-import java.util.ArrayList;
 
 public class ShowResult extends JFrame {
     JPanel showResult;
-    public ShowResult(ArrayList<String> result) {
+    public ShowResult() {
         super("获奖者名单");
         this.setVisible(true);
         //根据当前集合内的数据调整窗体的大小
-        this.setSize(250, 40 + result.size() * 40);
+        this.setSize(250, 40 + Data.allWinner.size() * 40);
         //使窗口居中显示
         this.setLocation(Setting.toolkit.setCentered(this));
         this.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
         //总面板
-        showResult = new JPanel(new GridLayout(result.size(), 0,1,1));
+        showResult = new JPanel(new GridLayout(Data.allWinner.size(), 0,1,1));
         showResult.setBackground(new Color(154, 154, 154));
         //添加内容
-        for (String s : result) {
+        for (String s : Data.allWinner) {
             //内部面板 每一个号码都会单独一个面板内
             JPanel panel = new JPanel(new BorderLayout());
             JLabel text = new JLabel(s, JLabel.CENTER);
